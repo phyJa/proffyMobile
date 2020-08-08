0eb1fc79a50c08f5aff16266ba28daca65d1d24e
Elias <eli.bueno9@gmail.com>
2020-08-07 21:09:08 -0300

    Adding images to the project

--- App.tsx
+++ App.tsx
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>dsadsadasd</Text>
+      <Text style={styles.text}>Hello, NLW!!</Text>
+      <Text style={styles.text}>Some other text</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +14,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, // Note that in React Native, the flex direction standard is the 'column' type
+    backgroundColor: 'rgb(42, 9, 9)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: 'white',
+    fontSize: 36,
+    fontWeight: 'bold'
+  }
 });
