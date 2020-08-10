30067e5f17ecf3c8974345721f6f88b1b2f6f256
Elias <eli.bueno9@gmail.com>
2020-08-10 10:28:45 -0300

    Starting to insert navigation

---
+++ src/routes/AppStack.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native"; // An analogue to the Browser Router (Web)
+import { createStackNavigator } from "@react-navigation/stack"; // The stack navigation
+
+// Pages
+import Landing from '../pages/Landing';
+
+const { Navigator, Screen } = createStackNavigator();
+
+function AppStack () {
+    return(
+        <NavigationContainer>
+            <Navigator>
+                <Screen name="Landing" component={Landing} />
+            </Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default AppStack;
