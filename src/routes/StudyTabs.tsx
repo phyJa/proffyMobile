d2d5fa87514c73a53d770bf954f5c00156cae905
Elias <eli.bueno9@gmail.com>
2020-08-11 11:52:55 -0300

    Styling the tabs through the tabBarOptions property in Navigator

--- src/routes/StudyTabs.tsx
+++ src/routes/StudyTabs.tsx
@@ -9,7 +9,41 @@ const {Navigator, Screen} = createBottomTabNavigator();
 
 export default function StudyTabs() {
     return (
-        <Navigator>
+        <Navigator
+            tabBarOptions={{
+                style: {
+                    elevation: 0, //This is to eliminate a shadow at the top of the tabs in Android
+                    shadowOpacity: 0, // The same as the latter, but for IOS
+                    height: 64
+                },
+
+                tabStyle: {
+                    flexDirection: "row",
+                    alignItems:"center",
+                    justifyContent: 'center'
+                },
+
+                iconStyle: {
+                    flex: 0,
+                    width: 20,
+                    height: 20
+                },
+
+                labelStyle: { // Text style
+                    fontFamily: "Archivo_700Bold",
+                    fontSize: 13,
+                    marginLeft: 16
+                },
+
+                inactiveBackgroundColor: "#fafafc",
+
+                activeBackgroundColor: "#ebebf5",
+
+                inactiveTintColor: "#c1bccc",
+
+                activeTintColor: "#32264d"
+            }}
+        >
             <Screen name="TeacherList" component={TeacherList} />
             <Screen name="Favorites" component={Favorites} />
         </Navigator>
