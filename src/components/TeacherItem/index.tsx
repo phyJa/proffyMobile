473105509808fd5974562adbb00d23aa73433adb
Elias <eli.bueno9@gmail.com>
2020-08-12 11:38:09 -0300

    Startin to work with TeacherItem

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+
+// Style
+import styles from './styles';
+
+function TeacherItem() {
+    return (
+        <View />
+    );
+}
+
+export default TeacherItem;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,15 +3,17 @@ import { View, Text } from 'react-native';
 
 // Components
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 // Style
 import styles from './styles';
-
 
 function TeacherList () {
     return (
         <View style={styles.container}>
             <PageHeader title="Available Proffys" />
+
+            <TeacherItem />
         </View>
     );
 }
