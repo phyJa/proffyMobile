4f9a7badc465937e33a0f58b0b2b0778fb83a3d3
Elias <eli.bueno9@gmail.com>
2020-08-13 09:44:44 -0300

    Page header input created

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, TextInput } from 'react-native';
 
 // Components
 import PageHeader from '../../components/PageHeader';
@@ -8,11 +8,44 @@ import TeacherItem from '../../components/TeacherItem';
 // Style
 import styles from './styles';
 
+
 function TeacherList () {
     return (
         <View style={styles.container}>
             <PageHeader title="Available Proffys">
-                <Text>Testing</Text>
+                <View style={styles.searchForm}>
+                    <Text style={styles.label}> Subject </Text>
+                    
+                    <TextInput
+                        placeholderTextColor="#c1bccc" 
+                        style={styles.input}
+                        placeholder="Which is the subject?"
+                    />
+
+                    <View style={styles.inputGroup}>
+                        <View style={styles.inputBlock}>
+                        
+                            <Text style={styles.label}> Week day </Text>
+                        
+                            <TextInput
+                                placeholderTextColor="#c1bccc" 
+                                style={styles.input}
+                                placeholder="Type the week day"
+                            />
+                        </View>
+
+                        <View style={styles.inputBlock}>
+                        
+                            <Text style={styles.label}> Time </Text>
+                        
+                            <TextInput
+                                placeholderTextColor="#c1bccc" 
+                                style={styles.input}
+                                placeholder="Type the time"
+                            />
+                        </View>
+                    </View>
+                </View>
             </PageHeader>
 
             <ScrollView
